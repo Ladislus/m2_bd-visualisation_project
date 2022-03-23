f19d34a2d655040bf8ab718e50b417799e8d310d
Ladislus <walcak.ladislas@gmail.com>
2022-03-23 16:25:36 +0100

    [ADD] Added OutOfCore

--- cmake-build-debug/config.h
+++
@@ -1,1 +0,0 @@
-#define MY_MESHES_PATH "/home/ladislus/Git/m2_bd-visualisation_project/data"
--- proj.cpp
+++ proj.cpp
@@ -1,7 +1,5 @@
 #include <string>
-#include <sstream>
 #include <iostream>
-#include <fstream>
 #include <vtkRenderWindow.h>
 #include <vtkRenderer.h>
 #include <vtkRenderWindowInteractor.h>
@@ -11,57 +9,24 @@
 #include <vtkLookupTable.h>
 #include <vtkRectilinearGrid.h>
 #include <vtkFloatArray.h>
-#include <vtkShortArray.h>
-#include <vtkUnsignedShortArray.h>
 #include <vtkCutter.h>
 #include <vtkPlane.h>
 #include <vtkPointData.h>
-#include <vtkGraphicsFactory.h>
-#include <vtkPNGWriter.h>
 #include <vtkImageData.h>
 #include <vtkCamera.h>
 #include <vtkContourFilter.h>
 #include <vtkProperty.h>
-#include <vtkVectorNorm.h>
 #include <ctime>
 #include <cmath>
-#include <vtkTransformPolyDataFilter.h>
 #include <vtkTransform.h>
 #include <vtkTransformFilter.h>
+#include <vtkPNGWriter.h>
+#include <vtkUnsignedShortArray.h>
 
 #include "config.h"
 
 #define BIG
 
-//*/
-#define FICHIER MY_MESHES_PATH "/Frog.raw_256_256_44_CHAR.raw"
-
-int gridSize = 256;
-int YgridSize = 256;
-int ZgridSize = 44;
-
-#define CHAR
-
-int startexploreval = 13;
-int endexploreval = 20;
-
-//*/
-
-/*/
-#define FICHIER MY_MESHES_PATH "/Mystere5_2048_2048_756_SHORT.raw"
- 
- int gridSize = 2048;
- int YgridSize = 2048;
- int ZgridSize = 756;
-
- #define SHORT
-
-int startexploreval=1;
-int endexploreval=65000;
-
- //*/
-
-/*/
 #define FICHIER MY_MESHES_PATH "/Mystere6_1118_2046_694_CHAR.raw"
 
 int gridSize = 1118;
@@ -70,187 +35,176 @@ int ZgridSize = 694;
 
 #define CHAR
 
-int startexploreval=1;
-int endexploreval=255;
-
-//*/
-
-
-/*
-#define FICHIER MY_MESHES_PATH "/Mystere1_512_512_134_SHORT.raw"
-int gridSize = 512;
-int YgridSize = 512;
-int ZgridSize = 134;
- 
- #define SHORT
-
- int startexploreval=1;
- int endexploreval=65000;
-//*/
-
-
-/*
-#define FICHIER MY_MESHES_PATH "/Mystere4_512_512_322_SHORT.raw"
-int gridSize = 512;
-int YgridSize = 512;
-int ZgridSize = 322;
- 
- #define SHORT
-
- int startexploreval=1;
- int endexploreval=65000;
-//*/
-
+int startexploreval = 26;
+int endexploreval = 46;
 
 const char *location = FICHIER;
 
 int winSize = 500;
-int numPasses = 10;
-int nbimages = 10;
+int numPasses = 20;
+int passNum = 0;
 
 const char *prefix = "";
-int passNum = 0;
 
 using std::cerr;
 using std::endl;
 
 // Function prototypes
 vtkRectilinearGrid *ReadGrid(int zStart, int zEnd);
+
 void WriteImage(const char *name, const float *rgba, int width, int height);
+
 bool ComposeImageZbuffer(float *rgba_out, float *zbuffer, int image_width, int image_height);
 
+int main() {
 
-int main(int argc, char *argv[]) {
 	srand(time(nullptr));
-//    int zStart = 0;
-//    int zEnd = ZgridSize;
-
-	int npixels = winSize * winSize;
-	vtkRectilinearGrid *reader = nullptr;
-
 
 	vtkLookupTable *lut = vtkLookupTable::New();
-
 	lut->SetHueRange(0.1, 0.0);
 	lut->SetSaturationRange(0.0, 1.0);
 	lut->SetValueRange(1.0, 255.0);
 	lut->SetNumberOfColors(100);
 	lut->Build();
 
-
 	vtkRenderer *ren = vtkRenderer::New();
 	double bounds[6] = {0.00001, 1 - 0.00001, 0.00001, 1 - 0.00001, 0.00001, 1 - 0.00001};
-
 	ren->ResetCamera(bounds);
 
-	bool once = true;
+	//bool once = true;
 	vtkRenderWindow *renwin = vtkRenderWindow::New();
 	renwin->SetSize(winSize, winSize);
-
 	renwin->AddRenderer(ren);
 
-
 	int zStart = 0;
-
-
 	int zEnd = ZgridSize;
 
-
-	reader = ReadGrid(zStart, zEnd);
+	vtkRectilinearGrid *reader;
+	//vtkRectilinearGrid *reader = ReadGrid(zStart, zEnd);
 
 	vtkContourFilter *cf = vtkContourFilter::New();
 	cf->SetNumberOfContours(1);
-	// cf->SetValue(0, 20.0);
 	int valcont = startexploreval;
 	cf->SetValue(1, valcont);
-
 	cf->SetInputData(reader);
 
 	int maxsize = std::max(gridSize, std::max(YgridSize, ZgridSize));
 	vtkSmartPointer<vtkTransform> transform = vtkSmartPointer<vtkTransform>::New();
-	transform->Scale(gridSize / (float) maxsize, YgridSize / (float) maxsize, ZgridSize / (float) maxsize);
+	//transform->Scale(
+	//		(float) gridSize / (float) maxsize,
+	//		(float) YgridSize / (float) maxsize,
+	//		(float) ZgridSize / (float) maxsize
+	//);
+	transform->Scale(1.f, 1.f, 1.f);
 	vtkSmartPointer<vtkTransformFilter> transformFilter = vtkSmartPointer<vtkTransformFilter>::New();
 	transformFilter->SetInputConnection(cf->GetOutputPort());
 	transformFilter->SetTransform(transform);
 
 	vtkDataSetMapper *mapper = vtkDataSetMapper::New();
-	//     mapper->SetInputData( reader );
 	mapper->SetInputConnection(transformFilter->GetOutputPort());
 
 	vtkActor *actor = vtkActor::New();
 	actor->SetMapper(mapper);
 
-	float startval = valcont;
-	float endval = endexploreval;
-
 	mapper->SetScalarRange(startexploreval, endexploreval);
-
 	mapper->SetLookupTable(lut);
 
 	ren->AddActor(actor);
 	ren->SetViewport(0, 0, 1, 1);
 
-
-	// vtkCamera *cam = ren->GetActiveCamera();
 	vtkCamera *cam;
-//            if(once){
-//                once=false;
-//                cam= ren->GetActiveCamera();
-//             cam->SetFocalPoint(0.5, 0.5, 0.5);
-//                cam->SetPosition(0., .0, 3.);
-//                cam->SetViewUp(0., -1.0, 0.0);
-//                bounds=ren->ComputeVisiblePropBounds();
-//                std::cout<<"bounds : "<<bounds[0]<<" ; "<<bounds[1]<<" ; "<<bounds[2]<<" ; "<<bounds[3]<<" ; "<<bounds[4]<<" ; "<<bounds[5]<<" ; "<<endl<<fflush;
-//            }
-//            else{ren->SetActiveCamera(cam);
-//               
-//            }
+	cam = ren->GetActiveCamera();
+	cam->SetFocalPoint(0.5, 0.5, 0.5);
+	cam->SetPosition(-0., .0, 3.);
+	cam->SetViewUp(0., -1.0, 0.0);
+	//bounds = ren->ComputeVisiblePropBounds();
 
-	// cam= ren->GetActiveCamera();
-	//cam->SetFocalPoint(0.5, 0.5, 0.5);
-	//cam->SetPosition(-0., .0, 3.);
-	//cam->SetViewUp(0., -1.0, 0.0);
-	//bounds=ren->ComputeVisiblePropBounds();
-	// std::cout<<"bounds : "<<bounds[0]<<" ; "<<bounds[1]<<" ; "<<bounds[2]<<" ; "<<bounds[3]<<" ; "<<bounds[4]<<" ; "<<bounds[5]<<" ; "<<endl<<fflush;
+	float *rgba = nullptr;
+	auto *auxrgba = new float[4 * winSize * winSize];
+	auto *auxzbuffer = new float[4 * winSize * winSize];
 
-	//       ren->SetActiveCamera (cam);
-	//           cam= ren->GetActiveCamera();
-	//   cam->SetPosition(0.5, 3.0, 0.5);
-	//  cam->SetViewUp(0., 0.0, 1.0);
-	//      cam->SetFocalPoint(0., 0.0, 0.);
+	for (int i = 0; i < winSize * winSize; i++) {
+		auxzbuffer[i] = 1.0;
+		auxrgba[i * 4] = 0;
+		auxrgba[i * 4 + 1] = 0;
+		auxrgba[i * 4 + 2] = 0;
+		auxrgba[i * 4 + 3] = 0;
+	}
 
-//        
+	const int rst = ZgridSize % numPasses;
+	const int step = ZgridSize / numPasses;
+	for (passNum = 0; passNum < numPasses; passNum++) {
+		zStart = passNum * step + (passNum < rst ? passNum : rst);
+		zEnd = (passNum + 1) * step + ((passNum + 1) < rst ? passNum + 1 : rst);
 
-	//*
+		std::cout << "Step: " << step << "; zStart: " << zStart << "; zEnd: " << zEnd << std::endl;
+
+		vtkRectilinearGrid *rg = ReadGrid(zStart, zEnd);
+
+		cf->SetInputData(rg);
+		rg->Delete();
+
+		// Force an update and set the parallel rank as the active scalars.
+		cf->Update();
+		cf->GetOutput()->GetPointData()->SetActiveScalars("pass_num");
+
+		renwin->Render();
+
+		rgba = renwin->GetRGBAPixelData(0, 0, winSize - 1, winSize - 1, 1);
+		float *zbuffer = renwin->GetZbufferData(0, 0, winSize - 1, winSize - 1);
+
+		for (int i = 0; i < winSize * winSize; i++) {
+			if (auxzbuffer[i] > zbuffer[i]) {
+				auxzbuffer[i] = zbuffer[i];
+				auxrgba[i * 4] = rgba[i * 4];
+				auxrgba[i * 4 + 1] = rgba[i * 4 + 1];
+				auxrgba[i * 4 + 2] = rgba[i * 4 + 2];
+				auxrgba[i * 4 + 3] = rgba[i * 4 + 3];
+			}
+		}
+
+		char name[128];
+		sprintf(name, "image%d.png", passNum);
+		WriteImage(name, rgba, winSize, winSize);
+
+		auto *new_rgba = new float[4 * winSize * winSize];
+		bool didComposite = ComposeImageZbuffer(new_rgba, zbuffer, winSize, winSize);
+
+		char namez[128];
+		sprintf(namez, "imageZ%d.png", passNum);
+		WriteImage(namez, new_rgba, winSize, winSize);
+
+
+		free(rgba);
+		free(zbuffer);
+		free(new_rgba);
+	}
+
+	WriteImage("final_image.png", auxrgba, winSize, winSize);
+	free(auxrgba);
+
+	reader = ReadGrid(0, ZgridSize);
+	cf->SetInputData(reader);
+	cf->Update();
+
 	vtkRenderWindowInteractor *iren = vtkRenderWindowInteractor::New();
 	iren->SetRenderWindow(renwin);
 	renwin->Render();
 	iren->Start();
-	//*/
 
 	reader->Delete();
 	mapper->Delete();
 	cf->Delete();
-	//          lut->Delete();
+	lut->Delete();
 	ren->RemoveActor(actor);
 	actor->Delete();
-
 	ren->Delete();
 	renwin->Delete();
 }
 
-// You should not need to modify these routines.
-
-vtkRectilinearGrid *
-ReadGrid(int zStart, int zEnd) {
+vtkRectilinearGrid *ReadGrid(int zStart, int zEnd) {
 	int i;
 
-	/*   if (zStart < 0 || zEnd < 0 || zStart >= gridSize || zEnd >= gridSize || zStart > zEnd)
-	 {
-	 cerr << prefix << "Invalid range: " << zStart << "-" << zEnd << endl;
-	 return NULL;
-	 }
-	 */
 	std::ifstream ifile(location);
 	if (ifile.fail()) {
 		cerr << prefix << "Unable to open file: " << location << "!!" << endl;
@@ -285,7 +239,7 @@ ReadGrid(int zStart, int zEnd) {
 	unsigned int valuesPerSlice = gridSize * YgridSize;
 
 #if defined(SHORT)
-	unsigned int bytesPerSlice   = sizeof(short)*valuesPerSlice;
+	unsigned int bytesPerSlice = sizeof(short) * valuesPerSlice;
 
 #elif defined(CHAR)
 	unsigned int bytesPerSlice = sizeof(char) * valuesPerSlice;
@@ -299,9 +253,9 @@ ReadGrid(int zStart, int zEnd) {
 
 
 #if defined(SMALL)
-	unsigned int offset          = (unsigned int)zStart * (unsigned int)bytesPerSlice;
-   unsigned int bytesToRead     = bytesPerSlice*numSlicesToRead;
-   unsigned int valuesToRead    = valuesPerSlice*numSlicesToRead;
+	unsigned int offset      = (unsigned int) zStart * (unsigned int) bytesPerSlice;
+   unsigned int bytesToRead  = bytesPerSlice*numSlicesToRead;
+   unsigned int valuesToRead = valuesPerSlice*numSlicesToRead;
 #elif defined(BIG)
 	unsigned long long offset = (unsigned long long) zStart * bytesPerSlice;
 	unsigned long long bytesToRead = (unsigned long long) bytesPerSlice * numSlicesToRead;
@@ -310,17 +264,14 @@ ReadGrid(int zStart, int zEnd) {
 #error Unsupported choice setting
 #endif
 
-
 #if defined(SHORT)
 	vtkUnsignedShortArray *scalars = vtkUnsignedShortArray::New();
 	scalars->SetNumberOfTuples(valuesToRead);
 	unsigned short *arr = scalars->GetPointer(0);
-
 #elif defined(CHAR)
 	vtkUnsignedCharArray *scalars = vtkUnsignedCharArray::New();
 	scalars->SetNumberOfTuples(valuesToRead);
 	unsigned char *arr = scalars->GetPointer(0);
-
 #elif  defined(FLOAT)
 	vtkFloatArray *scalars = vtkFloatArray::New();
 	scalars->SetNumberOfTuples(valuesToRead);
@@ -344,14 +295,11 @@ ReadGrid(int zStart, int zEnd) {
 
 		if (rand() % (valuesToRead / 20) == 0) {
 #if defined(SHORT)
-			std::cout<<(unsigned short)(scalars->GetPointer(0))[i]<<" ";
-
+			std::cout << (unsigned short) (scalars->GetPointer(0))[i] << " ";
 #elif defined(CHAR)
 			std::cout << +(unsigned char) (scalars->GetPointer(0))[i] << " ";
-
 #elif  defined(FLOAT)
 			std::cout<<(float)(scalars->GetPointer(0))[i]<<" ";
-
 #else
 #error Unsupported choice setting
 #endif
@@ -385,7 +333,6 @@ ReadGrid(int zStart, int zEnd) {
 	return rg;
 }
 
-
 void WriteImage(const char *name, const float *rgba, int width, int height) {
 	vtkImageData *img = vtkImageData::New();
 	img->SetDimensions(width, height, 1);
@@ -395,10 +342,9 @@ void WriteImage(const char *name, const float *rgba, int width, int height) {
 #else
 	img->AllocateScalars(VTK_UNSIGNED_CHAR, 3);
 #endif
-
 	for (int i = 0; i < width; i++)
 		for (int j = 0; j < height; j++) {
-			unsigned char *ptr = (unsigned char *) img->GetScalarPointer(i, j, 0);
+			auto *ptr = (unsigned char *) img->GetScalarPointer(i, j, 0);
 			int idx = j * width + i;
 			ptr[0] = (unsigned char) (255 * rgba[4 * idx + 0]);
 			ptr[1] = (unsigned char) (255 * rgba[4 * idx + 1]);
